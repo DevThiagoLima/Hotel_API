3fc20626ca4fde67a3cb7faf70751b25ccf6eb18
Thiago Lima <thiagolima8008@gmail.com>
2026-03-21 12:15:46 -0300

    Commit Inicial - SpringBoot

---
+++ src/main/java/com/hotel/hotelaria/HotelariaApplication.java
@@ -0,0 +1,14 @@
+package com.hotel.hotelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelariaApplication.class, args);
+
+
+	}
+}
---
+++ src/test/java/com/hotel/hotelaria/HotelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.hotelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
